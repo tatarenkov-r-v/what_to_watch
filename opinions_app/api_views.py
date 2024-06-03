7e1c05d7b38bfe27f3f75e423821871566a97311
Roman <flexomj@gmail.com>
2024-06-03 17:33:03 +0300

    update

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -6,17 +6,31 @@ from . import app, db
 from .models import Opinion
 from .views import random_opinion
 
+# Импорт исключения.
+from .error_handlers import InvalidAPIUsage
+
 # Явно разрешаем метод GET:
 @app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
-    opinion = Opinion.query.get_or_404(id)
-    # Никаких лишних функций, просто метод to_dict():
+    opinion = Opinion.query.get(id)
+    if opinion is None:
+        # Тут код ответа нужно указать явным образом.
+        raise InvalidAPIUsage('Мнение с указанным id не найдено', 404)
     return jsonify({'opinion': opinion.to_dict()}), 200
 
 
 @app.route('/api/opinions/<int:id>/', methods=['PATCH'])
 def update_opinion(id):
     data = request.get_json()
+    if (
+        'text' in data and
+        Opinion.query.filter_by(text=data['text']).first() is not None
+    ):
+        # При неуникальном значении поля text
+        # возвращаем сообщение об ошибке в формате JSON
+        # и статус-код 400:
+        raise InvalidAPIUsage('Такое мнение уже есть в базе данных')
+
     # Если метод get_or_404 не найдёт указанный ID,
     # то он выбросит исключение 404:
     opinion = Opinion.query.get_or_404(id)
@@ -55,6 +69,17 @@ def get_opinions():
 def add_opinion():
     # Получение данных из запроса в виде словаря:
     data = request.get_json()
+    if 'title' not in data or 'text' not in data:
+        # Выбрасываем собственное исключение.
+        # Второй параметр (статус-код) в этом обработчике можно не передавать:
+        # нужно вернуть код 400, а он и так возвращается по умолчанию.
+        raise InvalidAPIUsage('В запросе отсутствуют обязательные поля')
+    # Если в базе данных уже есть объект
+    # с таким же значением поля text...
+    if Opinion.query.filter_by(text=data['text']).first() is not None:
+        # ...возвращаем сообщение об ошибке в формате JSON
+        # и статус-код 400:
+        raise InvalidAPIUsage('Такое мнение уже есть в базе данных')
     # Создание нового пустого экземпляра модели:
     opinion = Opinion()
     # Наполнение экземпляра данными из запроса:
@@ -69,4 +94,6 @@ def add_opinion():
 @app.route('/api/get-random-opinion/', methods=['GET'])
 def get_random_opinion():
     opinion = random_opinion()
-    return jsonify({'opinion': opinion.to_dict()}), 200
+    if opinion is not None:
+        return jsonify({'opinion': opinion.to_dict()}), 200
+    raise InvalidAPIUsage('В базе данных нет мнений', 404)
