37d3c42e8d2d58c55df4b3df44077bf998a0c371
Roman <flexomj@gmail.com>
2024-06-03 16:44:34 +0300

    update

--- opinions_app/__init__.py
+++ opinions_app/__init__.py
@@ -3,9 +3,14 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from settings import Config
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 from . import api_views, cli_commands, error_handlers, views
+
+with app.app_context():
+    db.create_all()
